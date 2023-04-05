695856de2f5a868817576e0274f4c8f6601fd04c
Дмитрий <dimalyamov@yandex.ru>
2023-04-06 00:06:14 +0300

    Правки

--- main/src/main/java/ru/practicum/main/MainApp.java
+++ main/src/main/java/ru/practicum/main/MainApp.java
@@ -3,8 +3,9 @@ package ru.practicum.main;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 
+@SpringBootApplication
 public class MainApp {
     public static void main(String[] args) {
-        SpringApplication.run(MainServerApp.class, args);
+        SpringApplication.run(MainApp.class, args);
     }
 }
