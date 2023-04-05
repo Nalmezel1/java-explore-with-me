95629605a2b91deb3d15582d0fd02c39086d235b
Дмитрий <dimalyamov@yandex.ru>
2023-04-06 00:00:05 +0300

    Правки

---
+++ main/src/main/java/ru/practicum/main/MainApp.java
@@ -0,0 +1,10 @@
+package ru.practicum.main;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+public class MainApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MainServerApp.class, args);
+    }
+}
